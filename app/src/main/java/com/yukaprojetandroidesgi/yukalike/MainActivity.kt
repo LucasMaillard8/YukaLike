5ff35998793b50599d8a605638c0e970bb596c4e
NGUYEN, Cindy <cnguyen@tf1.fr>
2021-06-20 23:23:06 +0200

    Sign in with email

--- app/src/main/java/com/yukaprojetandroidesgi/yukalike/MainActivity.kt
+++ app/src/main/java/com/yukaprojetandroidesgi/yukalike/MainActivity.kt
@@ -1,17 +1,83 @@
 package com.yukaprojetandroidesgi.yukalike
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.database.ktx.database
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val database = Firebase.database
-        val myRef = database.getReference("message")
+        auth = Firebase.auth
+        val signInWithEmailButton: Button = findViewById(R.id.signEmailButton)
+        val email: EditText = findViewById(R.id.email)
+        val password: EditText = findViewById(R.id.password)
 
-        myRef.setValue("Hello, World!")
+        signInWithEmailButton.setOnClickListener {
+            print("Button pressed")
+            signIn(email.text.toString(), password.text.toString())
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun signIn(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "signInWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun createUser(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun reload() {
+        //TODO
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+        val homeActivityIntent: Intent = Intent(this, HomeActivity::class.java)
+        startActivity(homeActivityIntent)
     }
 }
