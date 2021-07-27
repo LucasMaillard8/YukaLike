dcc984f51c1c75151d4d968b7d6b17a96fb20812
Lucas Maillard <lucas.maillard8@gmail.com>
2021-07-27 10:50:46 +0200

    myAccount: email + progressBar

--- app/src/main/java/com/yukaprojetandroidesgi/yukalike/MyAccountFragment.kt
+++ app/src/main/java/com/yukaprojetandroidesgi/yukalike/MyAccountFragment.kt
@@ -11,21 +11,30 @@ import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.yukaprojetandroidesgi.yukalike.databinding.FragmentInfoProductBinding
+import com.yukaprojetandroidesgi.yukalike.databinding.FragmentMyAccountBinding
 import com.yukaprojetandroidesgi.yukalike.databinding.FragmentSignUpBinding
 import kotlinx.android.synthetic.main.fragment_my_account.*
 
-class MyAccountFragment : Fragment() {
+class MyAccountFragment : Fragment(R.layout.fragment_my_account) {
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var binding: FragmentMyAccountBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val myEmail = view?.findViewById<TextView>(R.id.myEmail)
-        myEmail?.text = getUserEmail()
-        return inflater.inflate(R.layout.fragment_my_account, container, false)
+        binding = FragmentMyAccountBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        //val myEmail = view.findViewById<TextView>(R.id.myEmail)
+        binding.myEmail.text = getUserEmail()
+        binding.progressBar.progress = 1
+
+        super.onViewCreated(view, savedInstanceState)
     }
 
     fun getUserEmail(): String {
